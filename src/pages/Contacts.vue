65d4423169270e9be13e3801fd4a87d8b60bcf1d
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-04-26 21:56:55 +0200

    third

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>Contacts</div>
+  <div class='container'>
+    <div class ='row'>
+      <div class ='col'>
+        <ContactList />
+      </div>
+      <div class = 'col'>
+        <ContactDetails />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ContactList from '../components/ContactList.vue'
+import ContactDetails from '../components/ContactDetails.vue'
+
 export default {
+  components:{
+    ContactList,
+    ContactDetails
+  }
 
 }
 </script>

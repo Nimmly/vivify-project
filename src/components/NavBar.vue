b79eab53efcb8038259191dbcf4741e5a112467c
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-04-26 21:45:48 +0200

    second

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <router-link class="navbar-brand" to="contacts">My Contacts</router-link>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <router-link class="nav-link " to="contacts">Contacts</router-link>
+      </li>
+      <li class="nav-item">
+      <router-link class="nav-link " to="add-contact">Add Contact</router-link>
+      </li> 
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/pages/AddContact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Add Contacts</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
